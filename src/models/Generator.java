e558f3e4e13a9a4ccf3b0ecfc3dbf4177d4a7cd3
S-A-3-D-O-U-N-E <112890463+saadMeddiche@users.noreply.github.com>
2023-09-15 20:47:44 +0100

    generate_repository (Only Insert)

--- src/models/Generator.java
+++ src/models/Generator.java
@@ -9,46 +9,129 @@ public class Generator {
     // public static String projectPath =
     // "C:\\Users\\YouCode\\Desktop\\generate-code\\Generator\\src\\projects\\products\\";
 
-    public static String projectPath = "C:\\Users\\Saad\\Desktop\\Generator-Code-Java\\src\\projects\\products\\";
+    public static String projectPath = "C:\\Users\\Saad\\Desktop\\testing-generated-code\\src\\";
 
     public static void main(String[] args) throws Exception {
-
         String NameOfModel = "Product";
         String[] NameOfAttributes = { "id", "name", "price" };
-        String[] TypeOfAttributes = { "Integer", "String", "Double" };
+        String[] TypeOfAttributes = { "int", "String", "Double" };
 
-        generate_CRUD(NameOfModel, NameOfAttributes, TypeOfAttributes);
-
+        generate_crud(NameOfModel, NameOfAttributes, TypeOfAttributes);
+        generate_repository(NameOfModel, NameOfAttributes, TypeOfAttributes);
     }
 
-    public static void generate_CRUD(String NameOfModel, String[] NameOfAttributes, String[] TypeOfAttributes)
+    public static void generate_crud(String NameOfModel, String[] NameOfAttributes, String[] TypeOfAttributes)
             throws Exception {
-        generate_Model(NameOfModel, NameOfAttributes, TypeOfAttributes);
+        generate_model(NameOfModel, NameOfAttributes, TypeOfAttributes);
     }
 
-    public static void generate_Model(String NameOfModel, String[] NameOfAttributes, String[] TypeOfAttributes)
+    public static void generate_model(String NameOfModel, String[] NameOfAttributes, String[] TypeOfAttributes)
             throws Exception {
 
+        // Importations
         String ModelCode = "package models;\n";
+
+        // Start
         ModelCode += "public class " + NameOfModel + " {\n";
 
+        // Fields
         for (int i = 0; i < NameOfAttributes.length; i++) {
             ModelCode += "    public " + TypeOfAttributes[i] + " " + NameOfAttributes[i] + ";\n";
         }
 
+        // Getters And Setters
         for (int i = 0; i < NameOfAttributes.length; i++) {
-            ModelCode += "    public " + TypeOfAttributes[i] + " get" + NameOfAttributes[i] + "() {\n";
+            ModelCode += "    public " + TypeOfAttributes[i] + " get" + BigFirstChar(NameOfAttributes[i]) + "() {\n";
             ModelCode += "        return " + NameOfAttributes[i] + ";\n";
             ModelCode += "    }\n";
-            
-            ModelCode += "    public void set" + NameOfAttributes[i] + "(" + TypeOfAttributes[i] + " "+ NameOfAttributes[i] + ") {\n";
+
+            ModelCode += "    public void set" + BigFirstChar(NameOfAttributes[i]) + "(" + TypeOfAttributes[i] + " "
+                    + NameOfAttributes[i] + ") {\n";
             ModelCode += "        this." + NameOfAttributes[i] + " = " + NameOfAttributes[i] + ";\n";
             ModelCode += "    }\n";
         }
 
+        // End
         ModelCode += "}\n";
 
         create_file(projectPath, "models/", NameOfModel + ".java", ModelCode);
+    }
+
+    public static void generate_repository(String NameOfModel, String[] NameOfAttributes, String[] TypeOfAttributes)
+            throws Exception {
+
+        String NameOfRepository = NameOfModel + "Repository";
+        String NameOfTable = NameOfModel.toLowerCase() + "s";
+
+        // Importations
+        String repositoryCode = "package repositories;\n";
+        repositoryCode += "import models." + NameOfModel + ";\n";
+        repositoryCode += "import java.sql.Connection;\n";
+        repositoryCode += "import java.sql.PreparedStatement;\n";
+        repositoryCode += "import java.sql.ResultSet;\n";
+        repositoryCode += "import java.sql.SQLException;\n";
+        repositoryCode += "import java.util.ArrayList;\n";
+        repositoryCode += "import java.util.List;\n";
+
+        // Start
+        repositoryCode += "public class " + NameOfRepository + " {\n";
+
+        // Fileds
+        repositoryCode += "private Connection connection;\n\n";
+
+        // Constructor
+        repositoryCode += "public " + NameOfRepository + "(Connection connection) {\n";
+        repositoryCode += "  this.connection = connection;\n";
+        repositoryCode += "}\n\n";
+
+        // Add method
+        repositoryCode += "public void create" + NameOfModel + "(";
+
+        // Parameters of function
+        repositoryCode += NameOfModel + " " + NameOfModel.toLowerCase();
+
+        repositoryCode += ") throws SQLException {\n";
+        repositoryCode += "String query = \"INSERT INTO " + NameOfTable + " (";
+
+        // Columns
+        for (int i = 0; i < NameOfAttributes.length; i++) {
+
+            String attributeName = NameOfAttributes[i];
+
+            repositoryCode += attributeName;
+            if (i < NameOfAttributes.length - 1) {
+                repositoryCode += ", ";
+            }
+        }
+
+        repositoryCode += ") VALUES (";
+
+        // ? ? ? ? ?
+        for (int i = 0; i < NameOfAttributes.length; i++) {
+            repositoryCode += "?";
+            if (i < NameOfAttributes.length - 1) {
+                repositoryCode += ", ";
+            }
+        }
+
+        repositoryCode += ")\";\n";
+
+        repositoryCode += "PreparedStatement preparedStatement = connection.prepareStatement(query);\n";
+
+        for (int i = 0; i < NameOfAttributes.length; i++) {
+            String attributeName = NameOfAttributes[i];
+            String attributeType = TypeOfAttributes[i];
+            repositoryCode += "            preparedStatement.set" + BigFirstChar(attributeType) + "(" + (i + 1) + ", "
+                    + NameOfModel.toLowerCase() + ".get" + BigFirstChar(attributeName) + "());\n";
+        }
+
+        repositoryCode += "            preparedStatement.executeUpdate();\n";
+        repositoryCode += "    }\n\n";
+
+        // End
+        repositoryCode += "}\n";
+
+        create_file(projectPath, "repositories/", NameOfRepository + ".java", repositoryCode);
     }
 
     public static void create_file(String projectPath, String folderName, String fileName, String content)
@@ -64,5 +147,9 @@ public class Generator {
         Path fullPath = Path.of(projectPath + folderName + fileName);
 
         Files.writeString(fullPath, content);
+    }
+
+    public static String BigFirstChar(String str) {
+        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
     }
 }
